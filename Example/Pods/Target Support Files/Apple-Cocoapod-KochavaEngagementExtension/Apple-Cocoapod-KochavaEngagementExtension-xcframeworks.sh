8169422930487d020d346582930307cdb1167594
John Bushnell <jbushnell@kochava.com>
2021-10-05 15:16:59 -0700

    5.0.0

--- Example/Pods/Target Support Files/Apple-Cocoapod-KochavaEngagementExtension/Apple-Cocoapod-KochavaEngagementExtension-xcframeworks.sh
+++ Example/Pods/Target Support Files/Apple-Cocoapod-KochavaEngagementExtension/Apple-Cocoapod-KochavaEngagementExtension-xcframeworks.sh
@@ -149,5 +149,5 @@ install_xcframework() {
   echo "Copied $source to $destination"
 }
 
-install_xcframework "${PODS_ROOT}/../../Apple-Cocoapod-KochavaEngagementExtension/Frameworks/KochavaEngagementExtension.xcframework" "KochavaEngagementExtension" "framework" "ios-arm64_armv7" "ios-arm64_x86_64-maccatalyst" "ios-arm64_i386_x86_64-simulator"
+install_xcframework "${PODS_ROOT}/../../Apple-Cocoapod-KochavaEngagementExtension/Frameworks/KochavaEngagementExtension.xcframework" "KochavaEngagementExtension" "framework" "ios-arm64_x86_64-maccatalyst" "ios-arm64_armv7" "ios-arm64_i386_x86_64-simulator"
 
